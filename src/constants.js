4a5ff983256d29d4aa19804ab929454870aede9a
MiguelDP4 <migueldp4@gmail.com>
2020-09-08 10:47:51 -0600

    Add INITIAL_ORDER_STATE to constants

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,15 @@
 export const CHANGE_FILTER = 'CHANGE_FILTER';
 export const CHANGE_ORDER = 'CHANGE_ORDER';
+export const POKEMON_TYPE = 'POKEMON_TYPE';
+export const POKEMON_NAME = 'POKEMON_NAME';
+export const DEX_NUMBER = 'DEX_NUMBER';
+const DEFAULT_SHOW_QUANTITY = 26;
 export const INITIAL_FILTER_STATE = {
   type: '',
   name: '',
+};
+export const INITIAL_ORDER_STATE = {
+  orderBy: DEX_NUMBER,
+  showQuantity: DEFAULT_SHOW_QUANTITY,
+  page: 1,
 };
