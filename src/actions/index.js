51c427c02d95e4b80562d0a2c5feead5168d5f24
MiguelDP4 <migueldp4@gmail.com>
2020-09-08 10:14:51 -0600

    add changeFilter and changeOrder actions

---
+++ src/actions/index.js
@@ -0,0 +1,23 @@
+/*objFilter is an object with various parameters
+The structure will be:
+{
+  type: pokemon_type,
+  name: snippet of the pokemon's name
+}*/
+export const changeFilter = objFilter => ({
+  type: CHANGE_FILTER,
+  filter: objFilter,
+});
+
+/*objOrder is an object with various parameters
+The structure will be:
+{
+  orderBy: parameterString,  (This could be type, name or pokedex number)
+  showQuantity: integer,     (This is the number of pokemon the user will see on their page)
+  page: integer,              (This is going to be used for pagination)
+}*/
+
+export const changeOrder = objOrder => ({
+  type: CHANGE_ORDER,
+  order: objOrder,
+});
