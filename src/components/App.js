ff8d0a88d41229407e4bdd28aee77ad46d282637
MiguelDP4 <migueldp4@gmail.com>
2020-09-09 15:32:43 -0600

    minor refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from '../logo.svg';
 import PokemonList from '../containers/PokemonList';
 
 function App() {
--- src/components/PokemonFilter.js
+++ src/components/PokemonFilter.js
@@ -3,16 +3,16 @@ import { PropTypes } from 'prop-types';
 import callAPI from '../async/fetchRequest';
 
 //This is for testing, API will fill this instead
-const PokemonTypes = callAPI('https://pokeapi.co/api/v2/type');
+// const PokemonTypes = callAPI('https://pokeapi.co/api/v2/type');
 
-console.log("pokemon types: " + PokemonTypes);
+// console.log("pokemon types: " + PokemonTypes);
 // .map(typeObject => String(typeObject.name))
 // .slice(0, 18);
 
 export default function PokemonFilter(props) {
-  const { handleChange } = props;
+  // const { handleChange } = props;
   return (
-    <select className="types-button" onChange={handleChange}>
+    <select className="types-button" >
       <option default value="All Pokemon">
         Pokemon Types
       </option>
@@ -25,6 +25,6 @@ export default function PokemonFilter(props) {
   );
 }
 
-PokemonFilter.propTypes = {
-  handleChange: PropTypes.func.isRequired,
-};
+// PokemonFilter.propTypes = {
+//   handleChange: PropTypes.func.isRequired,
+// };
