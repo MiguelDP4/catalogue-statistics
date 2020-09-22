3428445e7b9e0c90fb327a659b5c19d5b2166556
MiguelDP4 <migueldp4@gmail.com>
2020-09-22 11:44:34 -0600

    change pokemon card from button to div

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,14 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Pokemon = props => {
-  const { pokemonObject, handleClick } = props;
+  const { pokemonObject } = props;
   return (
-    <button
+    <div
       id={`pokemon-card-${pokemonObject.name}`}
       className="pokemon-card"
-      onClick={handleClick}
-      href=""
-      type="button"
     >
       <h3 className="pokemon-card-h3">{pokemonObject.name.toUpperCase()}</h3>
       <div className="image-container">
@@ -20,97 +17,7 @@ const Pokemon = props => {
         <span>{pokemonObject.types[0]}</span>
         <span>{pokemonObject.types[1] ? ` / ${pokemonObject.types[1]}` : ''}</span>
       </div>
-      <div className="stats-container">
-        <div className="stats-names">
-          <span className="stat-name">
-            HP:
-            {pokemonObject.stats.hp}
-          </span>
-          <span className="stat-name">
-            ATTACK:
-            {pokemonObject.stats.attack}
-          </span>
-          <span className="stat-name">
-            DEFENSE:
-            {pokemonObject.stats.defense}
-          </span>
-          <span className="stat-name">
-            SPECIAL-ATTACK:
-            {pokemonObject.stats.specialattack}
-          </span>
-          <span className="stat-name">
-            SPECIAL-DEFENSE:
-            {pokemonObject.stats.specialdefense}
-          </span>
-          <span className="stat-name">
-            SPEED:
-            {pokemonObject.stats.speed}
-          </span>
-        </div>
-        <div className="stats-bars-container">
-          <span className="stat-bar">
-            <div style={
-            {
-              height: '8px',
-              backgroundColor: '#dffffa',
-              width: `${pokemonObject.stats.hp / 2.55}%`,
-            }
-          }
-            />
-          </span>
-          <span className="stat-bar">
-            <div style={
-            {
-              height: '8px',
-              backgroundColor: '#dffffa',
-              width: `${pokemonObject.stats.attack / 2.55}%`,
-            }
-          }
-            />
-          </span>
-          <span className="stat-bar">
-            <div style={
-            {
-              height: '8px',
-              backgroundColor: '#dffffa',
-              width: `${pokemonObject.stats.defense / 2.55}%`,
-            }
-          }
-            />
-          </span>
-          <span className="stat-bar">
-            <div style={
-            {
-              height: '8px',
-              backgroundColor: '#dffffa',
-              width: `${pokemonObject.stats.specialattack / 2.55}%`,
-            }
-          }
-            />
-          </span>
-          <span className="stat-bar">
-            <div style={
-            {
-              height: '8px',
-              backgroundColor: '#dffffa',
-              width: `${pokemonObject.stats.specialdefense / 2.55}%`,
-            }
-          }
-            />
-          </span>
-          <span className="stat-bar">
-            <div style={
-            {
-              height: '8px',
-              backgroundColor: '#dffffa',
-              width: `${pokemonObject.stats.speed / 2.55}%`,
-            }
-          }
-            />
-          </span>
-        </div>
-      </div>
-    </button>
+  </div>
   );
 };
 
@@ -128,7 +35,6 @@ Pokemon.propTypes = {
       speed: PropTypes.number,
     }),
   }).isRequired,
-  handleClick: PropTypes.func.isRequired,
 };
 
 export default Pokemon;
